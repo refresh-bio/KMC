89a5a42954f94672d5b225cb61c988167d3d8442
Radoslaw Suchecki <radoslaw.suchecki@acpfg.com.au>
2016-02-26 16:35:40 +1030

    Relax regex for checking output filename to allow non [A-Za-z0-9_] chars such as directory separators

--- kmc_tools/parser.cpp
+++ kmc_tools/parser.cpp
@@ -29,7 +29,7 @@ CParser::CParser(const std::string& src):
 		exit(1);
 	}
 	//input_line_pattern = "\\s*(\\w*)\\s*=\\s*(.*)$";
-	input_line_pattern = "^\\s*([\\w-+]*)\\s*=\\s*(.*)$"; //TODO: consider valid file name	
+	input_line_pattern = "^\\s*(.*)\\s*=\\s*(.*)$"; //TODO: consider valid file name	
 	empty_line_pattern = "^\\s*$";
 }
 
