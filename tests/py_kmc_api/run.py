2d34af5d38bacceed786a012cd30df0d272cb080
marekkokot <marek.koki@gmail.com>
2018-12-29 20:28:14 +0100

    small fix to tests for py_kmc_api

--- tests/py_kmc_api/run.py
+++ tests/py_kmc_api/run.py
@@ -2,5 +2,5 @@
 import subprocess, sys, os
 #print(os.path.dirname(os.path.abspath(__file__)))
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-#subprocess.call(['python3', '-m', 'pytest'])
-subprocess.call(['pytest'])
+subprocess.call(['python3', '-m', 'pytest'])
+#subprocess.call(['pytest'])
