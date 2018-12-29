dcb2583fe1079964b40f21f2a6fa83ccb4935a77
marekkokot <marek.koki@gmail.com>
2018-12-29 20:49:50 +0100

    fix windows detection

--- tests/py_kmc_api/init_sys_path.py
+++ tests/py_kmc_api/init_sys_path.py
@@ -4,7 +4,7 @@ import os
 import platform
 import os
 def is_windows():
-    return 'win' in platform.system().lower()
+    return not is_mac() and 'win' in platform.system().lower()
 def is_linux():
     return 'linux' in platform.system().lower()
 def is_mac():
@@ -16,4 +16,4 @@ elif is_linux() or is_mac():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'bin'))
     pass
 else:
-    raise RuntimeError("system other than linux, mac os and windows was detected which is now not supported.")    
+    raise RuntimeError("system other than linux, mac os and windows was detected which is now not supported.")
